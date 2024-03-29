29a010e05ca8cd7388c7b82295b6375b4f5f02bf
namju <skawn1228@gmail.com>
2024-03-29 23:44:45 +0900

    feat: cards 컴포넌트 생성

---
+++ components/cards/cards.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import classes from "./cards.module.css";
+import Card from "../card/card";
+import { UserCard } from "@/app/folder/page";
+
+interface Props {
+  cards: UserCard[];
+  changeLinkDeleteSelect?: () => void;
+  changeFolderAddSelect?: () => void;
+}
+
+function Cards({
+  cards,
+  changeLinkDeleteSelect,
+  changeFolderAddSelect,
+}: Props) {
+  return (
+    <div className={classes.Cards}>
+      <div className={classes.container}>
+        {cards ? (
+          cards.map((card) => (
+            <Card
+              key={card.id}
+              card={card}
+              changeLinkDeleteSelect={changeLinkDeleteSelect}
+              changeFolderAddSelect={changeFolderAddSelect}
+            />
+          ))
+        ) : (
+          <p className={classes["no_link_msg"]}>저장된 링크가 없습니다.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
