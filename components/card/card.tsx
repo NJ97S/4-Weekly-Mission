3f3e91e4d8c53c350ea4271292a57a571c0e7e85
namju <skawn1228@gmail.com>
2024-03-30 00:07:09 +0900

    feat: card 컴포넌트 생성

---
+++ components/card/card.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState } from "react";
+import Image from "next/image";
+
+import classes from "./card.module.css";
+import { UserCard } from "@/app/folder/page";
+
+import elapsedTime from "@/utils/elapsedTime";
+import CardPopover from "../cardPopover/cardPopover";
+import { SampleCard } from "@/app/shared/page";
+
+import noCardImg from "../../assets/basic-card.png";
+import kebabIcon from "../../assets/kebab-icon.svg";
+
+interface Props {
+  card: UserCard | SampleCard;
+  changeLinkDeleteSelect?: () => void;
+  changeFolderAddSelect?: () => void;
+}
+
+function Card({ card, changeLinkDeleteSelect, changeFolderAddSelect }: Props) {
+  const [showPopover, setShowPopover] = useState(false);
+
+  const handleKebabClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setShowPopover(!showPopover);
+  };
+
+  return (
+    <div className={classes.Card}>
+      <a
+        href={card.url}
+        target="_blank"
+        rel="noreferrer noopener"
+        className={classes.card}
+      >
+        <div className={classes["card_image_container"]}>
+          <Image
+            className={classes["card_image"]}
+            src={(card.imageSource || card["image_source"]) ?? noCardImg}
+            alt={
+              card.imageSource || card["image_source"]
+                ? "card image"
+                : "no card image"
+            }
+          />
+        </div>
+        <div className={classes["card_information"]}>
+          <div className={classes["card_menu_container"]}>
+            <div className={classes.createdAt}>{elapsedTime(card)}</div>
+            <div className={classes["kebab_container"]}>
+              <Image
+                src={kebabIcon}
+                alt="kebab icon"
+                onClick={handleKebabClick}
+              />
+              {showPopover && (
+                <div className={classes["popover_container"]}>
+                  <CardPopover
+                    changeLinkDeleteSelect={changeLinkDeleteSelect}
+                    changeFolderAddSelect={changeFolderAddSelect}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+          <div className={classes.description}>{card.description}</div>
+          <div className={classes.createdAt}>
+            {new Date(
+              card.createdAt || card.created_at || 1
+            ).toLocaleDateString()}
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default Card;
