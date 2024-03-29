0aa2cc40a98bc25d5423fee1b04e95be8577c894
namju <skawn1228@gmail.com>
2024-03-29 23:37:30 +0900

    feat: linkAddBar 컴포넌트 생성

---
+++ components/linkAddBar/linkAddBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+
+import classes from "./linkAddBar.module.css";
+
+import linkIcon from "../../assets/link-icon.png";
+
+function LinkAddBar() {
+  return (
+    <div className={classes.LinkAddBar}>
+      <div className={classes.container}>
+        <Image
+          className={classes["link_icon"]}
+          src={linkIcon}
+          alt="link icon"
+        />
+        링크를 추가해보세요
+        <button className={classes["link_add_button"]}>추가하기</button>
+      </div>
+    </div>
+  );
+}
+
+export default LinkAddBar;
