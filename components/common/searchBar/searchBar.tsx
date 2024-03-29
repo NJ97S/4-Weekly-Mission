d4cc40aaa78f802634505d32b13716b5d84607cd
namju <skawn1228@gmail.com>
2024-03-30 00:48:20 +0900

    fix: searchBar 컴포넌트 내 이미지 경로 수정

--- components/common/searchBar/searchBar.tsx
+++ components/common/searchBar/searchBar.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Image from "next/image";
 
 import classes from "./searchBar.module.css";
 
-import searchBarImg from "../../assets/search-icon.png";
-import Image from "next/image";
+import searchBarImg from "@/public/images/search-icon.png";
 
 function SearchBar() {
   return (
